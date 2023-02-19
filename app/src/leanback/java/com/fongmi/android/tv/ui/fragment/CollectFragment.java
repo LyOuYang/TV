e950d9db16f06921557f07bef9e8b3fb0e1fca6f
Dehong <1252825428@qq.com>
2023-02-20 01:07:51 +0800

    优化搜索展示

--- app/src/leanback/java/com/fongmi/android/tv/ui/fragment/CollectFragment.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/fragment/CollectFragment.java
@@ -59,7 +59,7 @@ public class CollectFragment extends BaseFragment implements VodPresenter.OnClic
         CustomSelector selector = new CustomSelector();
         selector.addPresenter(ListRow.class, new CustomRowPresenter(16), VodPresenter.class);
         mBinding.recycler.setAdapter(new ItemBridgeAdapter(mAdapter = new ArrayObjectAdapter(selector)));
-        mBinding.recycler.setHeader(getActivity().findViewById(R.id.result), getActivity().findViewById(R.id.recycler));
+        mBinding.recycler.setHeader(getActivity().findViewById(R.id.result_layout), getActivity().findViewById(R.id.recycler));
         mBinding.recycler.setVerticalSpacing(ResUtil.dp2px(16));
         addVideo(Vod.arrayFrom(json));
     }
